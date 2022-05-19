7043fa7f5b54a5bd4d3e323a9f31588ca828e6a0
Pavel Gorbach <polgorbach@gmail.com>
2022-05-19 17:56:20 +0300

    implement filter

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useMemo, SyntheticEvent } from 'react'
 import useGetImages from '../../hooks/useGetImages'
 import Loader from '../Loader/Loader'
 import GalleryItem from '../GalleryItem/GalleryItem'
@@ -7,17 +7,35 @@ import styles from './Gallery.module.css'
 
 const Gallery = () => {
   const { loading, images } = useGetImages()
+  const [filter, setFilter] = useState('')
+
+  const filteredImages = useMemo(() => {
+    if(!filter) {
+      return images
+    }
+    return images.filter((image) => image.title.includes(filter)) 
+  }, [filter, images])
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => { 
+    setFilter(e.target.value)
+  } 
 
   if(loading) {
     return <Loader />
   }
 
   return (
-    <div className={styles.container}>
-      {images.map(({ id, thumbnailUrl, title }) => {
-        return <GalleryItem key={id} {...{thumbnailUrl, title}}  />
-      })}
+    <>
+    <div>
+      <span>SEARCH:</span>
+      <input type="text" name="name" value={filter} onChange={handleChange} />
     </div>
+      <div className={styles.container}>
+        {filteredImages.map(({ id, thumbnailUrl, title }) => {
+          return <GalleryItem key={id} {...{thumbnailUrl, title}}  />
+        })}
+      </div>
+    </>
   )
 }
 
